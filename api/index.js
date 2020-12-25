18ae7da7b29fb208b6d6df561248017d98a38be0
Joshua Costa <costa.joshua27@gmail.com>
2020-12-24 16:36:13 -0800

    Got mongoose connection working

--- api/index.js
+++ api/index.js
@@ -1,6 +1,26 @@
+// Require needed libraries for the server
 const express = require('express');
-const PORT = process.env.PORT;
+const http = require('http');
+
+// Initialize the server
 const app = express();
+const port = process.env.PORT;
+
+// Establish a database connection
+const mongoose = require('mongoose');
+const uri = process.env.DB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully!');
+});
+
+// Required middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+
+// Configure all routers
+
 
 app.get('/', (req, res) => {
   res.json({
@@ -8,4 +28,5 @@ app.get('/', (req, res) => {
   })
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// Have the server listen for requests on the designated port
+app.listen(port, () => console.log(`Listening on port ${port}`));
