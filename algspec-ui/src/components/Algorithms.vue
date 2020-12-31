65e4dc1ee45d5da7cee6a510a5cbede5bc64562b
Jhuangia <jialihuang007@gmail.com>
2020-12-31 10:58:33 -0800

    Added filtering by tags functionality for algorithms page

--- algspec-ui/src/components/Algorithms.vue
+++ algspec-ui/src/components/Algorithms.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <b-container>
-      <p v-for="algorithm in algorithms" :key="algorithm.name">
-        {{ algorithm.name }}
-        <router-link :to="{name: 'Algorithm', params: {name: algorithm.name}}">GO!</router-link>
-      </p>
+      <b-row v-for="tag in tags" :key="tag.name">
+        <b-button @click="addTag(tag); filterOut();">{{ tag.name }}</b-button>
+      </b-row>
+      <!-- <p>{{ selectedTags}}</p> -->
+    </b-container>
+    <hr>
+    <b-container>
+      <div v-if="algorithms !== null">
+        <p v-for="algorithm in shownAlgs" :key="algorithm.name">
+          {{ algorithm.name }}
+          <router-link :to="{name: 'Algorithm', params: {name: algorithm.name}}">GO!</router-link>
+        </p>
+      </div>
     </b-container>
   </div>
 </template>
@@ -18,14 +27,22 @@ export default {
   },
   data: function() {
     return {
-      tags: null, // might not need
+      tags: null,
       algorithms: null,
+      selectedTags: null,
+    }
+  },
+  computed: {
+    shownAlgs: function () {
+      return this.algorithms.filter(alg => alg.show);
     }
   },
   mounted: async function() {
     try {
       this.algorithms = (await api.get('/algorithm/all')).data;
-      this.tags = (await api.get('/tag/all')).data; // might not need
+      this.algorithms.forEach(alg => this.$set(alg, 'show', true)); // adds a new reactive property
+      this.tags = (await api.get('/tag/all')).data;
+      this.selectedTags = new Set();
       console.log('setting tags...', this.tags);
       console.log('setting algorithms...', this.algorithms);
     } catch (error) {
@@ -33,10 +50,20 @@ export default {
     }
   },
   methods: {
-
+    addTag(tag) { // should be toggleTag
+      this.selectedTags.has(tag.name) ? this.selectedTags.delete(tag.name) : this.selectedTags.add(tag.name);
+    },
+    async filterOut() {
+      const specifiedAlgs = [];
+      for (const tagName of this.selectedTags) {
+        const a = (await api.get(`/algorithm/tag/${tagName}`)).data;
+        specifiedAlgs.push(...a.map(alg=>alg.name));
+      }
+      const selectedAlgs = new Set(specifiedAlgs);
+      this.algorithms.forEach(alg => selectedAlgs.has(alg.name) ? alg.show = true : alg.show = false);
+    }
   }
-}
-
+};
 </script>
 
 <style>
