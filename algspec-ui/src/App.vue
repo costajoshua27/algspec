bb44d44644689485e0ec5b9bf6b396c4cda9785b
Jhuangia <jialihuang007@gmail.com>
2021-02-15 22:38:57 -0800

    Created styles directory and added initial background color and font to style (did very little)

--- algspec-ui/src/App.vue
+++ algspec-ui/src/App.vue
@@ -24,12 +24,12 @@ export default {
 }
 </script>
 
-<style>
-#app {
+<style src='@/styles/app.css'>
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+} */
 </style>
