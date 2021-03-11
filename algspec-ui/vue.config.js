0cc9ec53577132447effe8137a699680b9f91e67
Joshua Costa <costa.joshua27@gmail.com>
2021-03-11 00:38:58 -0800

    fixed current dockerfiles to allow hot reload

--- algspec-ui/vue.config.js
+++ algspec-ui/vue.config.js
@@ -9,6 +9,12 @@ module.exports = {
       }
     }
   },
+  devServer: {
+    port: 8081,
+    watchOptions: {
+      poll: true
+    }
+  }
   /*
   configureWebpack: {
     plugins: [new BundleAnalyzerPlugin()]
